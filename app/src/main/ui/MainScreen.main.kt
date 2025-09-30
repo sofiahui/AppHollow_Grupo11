d48c7c53981e5f683dbf36c793e16297eeb1646e
sofiahui <so.huichulef@duocuc.cl>
2025-09-30 20:15:51 -0300

    add code and rename

---
+++ app/src/main/ui/MainScreen.main.kt
@@ -0,0 +1,48 @@
+#!/usr/bin/env kotlin
+package com.example.apphollow_grupo11.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.apphollow_grupo11.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Mi App Kotlin") })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            Text(
+                text = "¡Bienvenido!",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Button(onClick = { /* acción futura */ }) {
+                Text(text = "Presióname")
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo App",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
+    }
+}
