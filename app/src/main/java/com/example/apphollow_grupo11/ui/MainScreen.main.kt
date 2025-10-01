cfefd96c66bd1ad537da7ebb39a8fa70e1e0e85b
sofiahui <so.huichulef@duocuc.cl>
2025-09-30 21:02:36 -0300

    add docs

--- app/src/main/java/com/example/apphollow_grupo11/ui/MainScreen.main.kt
+++ app/src/main/java/com/example/apphollow_grupo11/ui/MainScreen.main.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.apphollow_grupo11.R
 
@@ -36,7 +38,7 @@ fun MainScreen() {
             }
 
             Image(
-                painter = painterResource(id = R.drawable.logo),
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
                 contentDescription = "Logo App",
                 modifier = Modifier
                     .fillMaxWidth()
@@ -45,4 +47,10 @@ fun MainScreen() {
             )
         }
     }
+}
+
+@Preview()
+@Composable
+fun MainScreenPreview(){
+    MainScreen()
 }
